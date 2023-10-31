b1e2e6d135b50928389d817e0e2dda07962c57a6
thevineetdixit <148352948+thevineetdixit@users.noreply.github.com>
2023-10-31 18:34:48 +0530

    practice
    practicing coding

---
+++ elseif.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+//int number;
+//cin>>number;
+//if(number>0)
+//{cout<<"number is positive";}
+//
+//else if (number==0)
+//{cout<<"0";}
+//    
+//else 
+//{cout<<"number is negative";}
+
+
+
+
+char c;
+cin>>c;
+
+if (c=='a')
+cout<<"vowels";
+else if(c=='e')
+cout<<"vowels";
+else if(c=='i')
+cout<<"vowels";
+else if(c=='o')
+cout<<"vowels";
+else if(c=='u')
+cout<<"vowels";
+
+else(cout<<"consonents");
+
+}
---
+++ ifelse.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+//int percent;
+//cin>>percent;
+//if(percent<33)
+//{
+//    cout<<"Fail";
+//}
+//else
+//{
+//    cout<<"pass";
+//}
+//
+
+//or second method
+//int percent;
+//
+//cin>>percent;
+//if(percent<33)
+//cout<<"fail";
+//else
+//cout<<"pass";
+
+
+}
